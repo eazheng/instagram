454231f34c622619efe377df354c8f4b22bbbf14
Emily Zheng <eazheng@fb.com>
2019-07-08 15:16:39 -0700

    Initial Commit

---
+++ instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instagram
+//
+//  Created by eazheng on 7/8/19.
+//  Copyright © 2019 eazheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  instagram
+//
+//  Created by eazheng on 7/8/19.
+//  Copyright © 2019 eazheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
